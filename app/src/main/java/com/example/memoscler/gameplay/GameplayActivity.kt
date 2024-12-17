5a7233f7c52f754311b6f7fc587801f2d39de5c2
Justs <justsjeekabsons@gmail.com>
2024-12-17 21:26:27 +0200

    generateCoordinates function added

--- app/src/main/java/com/example/memoscler/gameplay/GameplayActivity.kt
+++ app/src/main/java/com/example/memoscler/gameplay/GameplayActivity.kt
@@ -3,11 +3,31 @@ package com.example.memoscler.gameplay
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.memoscler.R
+import com.example.memoscler.databinding.ActivityGameplayBinding
 
 class GameplayActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityGameplayBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityGameplayBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_gameplay)
+
+
     }
+
+    private fun generateCoordinates(gridSizeX: Int, gridSizeY: Int): List<Pair<Int, Int>> {
+        val coordinates = mutableListOf<Pair<Int, Int>>()
+        for (x in 0 until gridSizeX + 1){
+            for (y in 0 until gridSizeY + 1){
+                coordinates.add(Pair(x, y))
+            }
+        }
+        return coordinates
+    }
+
+
+
+
 }
