b0ef834fd33bdc67345bd2d8b51bc9c323f41581
Justs <justsjeekabsons@gmail.com>
2024-12-19 22:32:33 +0200

    function that generates cards based on CardModel now works. Also now there is an example functon that displays buttons on screen just for ilustration purposes.

--- app/src/main/java/com/example/memoscler/gameplay/GameplayActivity.kt
+++ app/src/main/java/com/example/memoscler/gameplay/GameplayActivity.kt
@@ -1,30 +1,62 @@
 package com.example.memoscler.gameplay
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.GridLayout
+import androidx.appcompat.app.ActionBar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import com.example.memoscler.R
 import com.example.memoscler.databinding.ActivityGameplayBinding
+import com.example.memoscler.models.CardModel
 
 class GameplayActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityGameplayBinding
+    lateinit var cards: MutableList<CardModel>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameplayBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_gameplay)
+        setContentView(binding.root)
+        binding.coordinateView.columnCount = 5
+        binding.coordinateView.rowCount = 5
 
+        cards = mutableListOf()
 
+        populateGridLayout(binding.coordinateView, 5, 5)
+        generateCardsAndTheirCoordinates(5,5)
     }
 
-    private fun generateCoordinates(gridSizeX: Int, gridSizeY: Int): List<Pair<Int, Int>> {
-        val coordinates = mutableListOf<Pair<Int, Int>>()
-        for (x in 0 until gridSizeX + 1){
-            for (y in 0 until gridSizeY + 1){
-                coordinates.add(Pair(x, y))
+    private fun populateGridLayout(gridLayout: GridLayout, gridSizeX: Int, gridSizeY: Int) {
+        for (x in 0 until gridSizeX) {
+            for (y in 0 until gridSizeY) {
+                val button = Button(this)
+                val params = GridLayout.LayoutParams().apply {
+                    columnSpec = GridLayout.spec(x)
+                    rowSpec = GridLayout.spec(y)
+                }
+                button.layoutParams = params
+                button.text = "($x, $y)"
+                gridLayout.addView(button)
             }
         }
-        return coordinates
+    }
+
+
+    private fun generateCardsAndTheirCoordinates(gridSizeX: Int, gridSizeY: Int) {
+        for (x in 1 until gridSizeX + 1){
+            for (y in 1 until gridSizeY + 1){
+                //coordinates.add(Pair(x, y))
+                val card = CardModel(
+                    coordinateX = x,
+                    coordinateY = y,
+                    state = false
+                )
+                cards.add(card)
+            }
+        }
+        Log.d("LIST OF CARDS", cards.toString())
     }
 
 
---
+++ app/src/main/java/com/example/memoscler/models/CardModel.kt
@@ -0,0 +1,7 @@
+package com.example.memoscler.models
+
+data class CardModel(
+    val coordinateX : Int,
+    val coordinateY : Int,
+    val state: Boolean
+)
